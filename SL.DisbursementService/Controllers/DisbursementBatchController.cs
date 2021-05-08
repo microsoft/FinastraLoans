a8722026de8dbca99e45707bd3937f0e2d80b6d0
Kartik Kanakasabesan <kartikka@microsoft.com>
2021-05-07 18:32:06 -0700

    initial update

---
+++ SL.DisbursementService/Controllers/DisbursementBatchController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SL.DisbursementService.Services;
+using SL.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SL.DisbursementService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DisbursementBatchController : ControllerBase
+    {
+        private readonly ILogger<DisbursementBatchController> _logger;
+        private readonly IDisbursementBatchRepository _disbursementBatchRepository;
+
+        public DisbursementBatchController(ILogger<DisbursementBatchController> logger, 
+            IDisbursementBatchRepository disbursementBatchRepository)
+        {
+            _logger = logger;
+            _disbursementBatchRepository = disbursementBatchRepository;
+        }
+
+        [HttpGet]
+        public Task<DisbursementBatch> Get()
+        {
+            return _disbursementBatchRepository.GetDisbursementBatch();
+        }
+    }
+}
---
+++ SL.DisbursementService/Controllers/DisbursementPaymentController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SL.DisbursementService.Services;
+using SL.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SL.DisbursementService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DisbursementPaymentController : ControllerBase
+    {
+        private readonly ILogger<DisbursementPaymentController> _logger;
+        private readonly IDisbursementPaymentRepository _disbursementPaymentRepository;
+
+        public DisbursementPaymentController(ILogger<DisbursementPaymentController> logger,
+            IDisbursementPaymentRepository disbursementPaymentRepository)
+        {
+            _logger = logger;
+            _disbursementPaymentRepository = disbursementPaymentRepository;
+        }
+
+        [HttpGet]
+        public Task<IEnumerable<DisbursementPayment>> Get(string batchName)
+        {
+            return _disbursementPaymentRepository.GetDisbursementPaymentByBatch(batchName);
+        }
+    }
+}
